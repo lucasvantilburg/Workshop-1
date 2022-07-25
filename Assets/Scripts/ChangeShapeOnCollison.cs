6f912f8f93670267486006511ba836476441ae19
lucasvantilburg <lucasvantilburg101202@gmail.com>
2022-07-25 20:58:06 +1000

    added balls and cube that change colours and shape

---
+++ Assets/Scripts/ChangeShapeOnCollison.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShapeOnCollison : MonoBehaviour
+{
+    Mesh mesh;
+
+    private void Start()
+    {
+        GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        this.mesh = Instantiate(obj.GetComponent<MeshFilter>().mesh);
+        Destroy(obj);
+    }
+
+
+    private void OnCollisionEnter()
+    {
+
+
+        var meshFilter = GetComponent<MeshFilter>();
+
+        if (this.mesh)
+        {
+            meshFilter.mesh = this.mesh;
+        }
+        
+    }
+}
